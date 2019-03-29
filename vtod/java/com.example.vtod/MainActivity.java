6c3d9046ba7e36764598218aefaa42f4a2d1b24c
luckynwj7 <luckynwj7@naver.com>
2019-03-29 15:02:28 +0900

    다시

--- vtod/java/com.example.vtod/MainActivity.java
+++ vtod/java/com.example.vtod/MainActivity.java
@@ -1,211 +1,351 @@
-#include <jni.h>
+﻿package com.example.vtod;
 
-#include <opencv2/opencv.hpp>
+import android.annotation.TargetApi;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.AssetManager;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
 
-#include <android/log.h>
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.imgcodecs.Imgcodecs;
+import org.opencv.imgproc.Imgproc;
 
-using namespace cv;
-using namespace std;
-
-//#include <string>
-//
-//extern "C" JNIEXPORT jstring JNICALL
-//Java_com_example_vtod_MainActivity_stringFromJNI(
-//        JNIEnv *env,
-//        jobject /* this */) {
-//    std::string hello = "Hello from C++";
-//    return env->NewStringUTF(hello.c_str());
-//}
-
-
-
-//extern "C"
-//JNIEXPORT void JNICALL
-//Java_com_example_vtod_MainActivity_ConvertRGBtoGray(JNIEnv *env, jobject instance,
-//                                                    jlong matAddrInput, jlong matAddrResult) {
-//
-//    // TODO
-//
-//    // 입력 RGBA 이미지를 GRAY 이미지로 변환
-//
-//    Mat &matInput = *(Mat *)matAddrInput;
-//
-//    Mat &matResult = *(Mat *)matAddrResult;
-//
-//
-//    cvtColor(matInput, matResult, COLOR_RGBA2GRAY);
-//
-//
-//
-//}
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.concurrent.Semaphore;
 
 
-float resize(Mat img_src, Mat &img_resize, int resize_width){
+public class MainActivity extends AppCompatActivity
+        implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    private static final String TAG = "opencv";
+    private CameraBridgeViewBase mOpenCvCameraView;
+    private Mat matInput;
+    private Mat matResult;
+
+    //public native void ConvertRGBtoGray(long matAddrInput, long matAddrResult);
+    public native long loadCascade(String cascadeFileName);
+    public native void detect(long cascadeClassifier_face,
+                              long cascadeClassifier_eye, long matAddrInput, long matAddrResult);
+    public long cascadeClassifier_face = 0;
+    public long cascadeClassifier_eye = 0;
 
 
-    float scale = resize_width / (float)img_src.cols ;
+    private final Semaphore writeLock = new Semaphore(1);
 
-    if (img_src.cols > resize_width) {
-
-        int new_height = cvRound(img_src.rows * scale);
-
-        resize(img_src, img_resize, Size(resize_width, new_height));
-
+    public void getWriteLock() throws InterruptedException {
+        writeLock.acquire();
     }
 
-    else {
-
-        img_resize = img_src;
-
+    public void releaseWriteLock() {
+        writeLock.release();
     }
 
-    return scale;
 
-}
-
-
-extern "C"
-JNIEXPORT jlong JNICALL
-Java_com_example_vtod_MainActivity_loadCascade(JNIEnv *env, jobject instance,
-                                               jstring cascadeFileName_) {
-    //const char *cascadeFileName = env->GetStringUTFChars(cascadeFileName_, 0);
-
-
-
-    //    const char *cascadeFileName = env->GetStringUTFChars(cascadeFileName_, 0);
-
-
-    const char *nativeFileNameString = env->GetStringUTFChars(cascadeFileName_, 0);
-
-
-    string baseDir("/storage/emulated/0/");
-
-    baseDir.append(nativeFileNameString);
-
-    const char *pathDir = baseDir.c_str();
-
-
-    jlong ret = 0;
-
-    ret = (jlong) new CascadeClassifier(pathDir);
-
-    if (((CascadeClassifier *) ret)->empty()) {
-
-        __android_log_print(ANDROID_LOG_DEBUG, "native-lib :: ",
-
-                            "CascadeClassifier로 로딩 실패  %s", nativeFileNameString);
-
+    static {
+        System.loadLibrary("opencv_java4");
+        System.loadLibrary("native-lib");
     }
 
-    else
+    private void copyFile(String filename) {
+        String baseDir = Environment.getExternalStorageDirectory().getPath();
+        String pathDir = baseDir + File.separator + filename;
 
-        __android_log_print(ANDROID_LOG_DEBUG, "native-lib :: ",
+        AssetManager assetManager = this.getAssets();
 
-                            "CascadeClassifier로 로딩 성공 %s", nativeFileNameString);
+        InputStream inputStream = null;
+        OutputStream outputStream = null;
 
+        try {
+            Log.d( TAG, "copyFile :: 다음 경로로 파일복사 "+ pathDir);
+            inputStream = assetManager.open(filename);
+            outputStream = new FileOutputStream(pathDir);
 
-
-    env->ReleaseStringUTFChars(cascadeFileName_, nativeFileNameString);
-
-
-    return ret;
-
-
-
-
-
-    // TODO
-
-    //env->ReleaseStringUTFChars(cascadeFileName_, cascadeFileName);
-}
-
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_com_example_vtod_MainActivity_detect(JNIEnv *env, jobject instance,
-                                          jlong cascadeClassifier_face, jlong cascadeClassifier_eye,
-                                          jlong matAddrInput, jlong matAddrResult) {
-
-    // TODO
-
-
-    Mat &img_input = *(Mat *) matAddrInput;
-
-    Mat &img_result = *(Mat *) matAddrResult;
-
-
-    img_result = img_input.clone();
-
-
-    std::vector<Rect> faces;
-
-    Mat img_gray;
-
-
-    cvtColor(img_input, img_gray, COLOR_BGR2GRAY);
-
-    equalizeHist(img_gray, img_gray);
-
-
-    Mat img_resize;
-
-    float resizeRatio = resize(img_gray, img_resize, 640);
-
-
-    //-- Detect faces
-
-    ((CascadeClassifier *) cascadeClassifier_face)->detectMultiScale( img_resize, faces, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-
-
-
-    __android_log_print(ANDROID_LOG_DEBUG, (char *) "native-lib :: ",
-
-                        (char *) "face %d found ", faces.size());
-
-
-    for (int i = 0; i < faces.size(); i++) {
-
-        double real_facesize_x = faces[i].x / resizeRatio;
-
-        double real_facesize_y = faces[i].y / resizeRatio;
-
-        double real_facesize_width = faces[i].width / resizeRatio;
-
-        double real_facesize_height = faces[i].height / resizeRatio;
-
-
-        Point center( real_facesize_x + real_facesize_width / 2, real_facesize_y + real_facesize_height/2);
-
-        ellipse(img_result, center, Size( real_facesize_width / 2, real_facesize_height / 2), 0, 0, 360,
-
-                Scalar(255, 0, 255), 30, 8, 0);
-
-
-
-        Rect face_area(real_facesize_x, real_facesize_y, real_facesize_width,real_facesize_height);
-
-        Mat faceROI = img_gray( face_area );
-
-        std::vector<Rect> eyes;
-
-
-        //-- In each face, detect eyes
-
-        ((CascadeClassifier *) cascadeClassifier_eye)->detectMultiScale( faceROI, eyes, 1.1, 2, 0 |CASCADE_SCALE_IMAGE, Size(30, 30) );
-
-
-        for ( size_t j = 0; j < eyes.size(); j++ )
-
-        {
-
-            Point eye_center( real_facesize_x + eyes[j].x + eyes[j].width/2, real_facesize_y + eyes[j].y + eyes[j].height/2 );
-
-            int radius = cvRound( (eyes[j].width + eyes[j].height)*0.25 );
-
-            circle( img_result, eye_center, radius, Scalar( 255, 0, 0 ), 30, 8, 0 );
-
+            byte[] buffer = new byte[1024];
+            int read;
+            while ((read = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, read);
+            }
+            inputStream.close();
+            inputStream = null;
+            outputStream.flush();
+            outputStream.close();
+            outputStream = null;
+        } catch (Exception e) {
+            Log.d(TAG, "copyFile :: 파일 복사 중 예외 발생 "+e.toString() );
         }
 
+    }
+
+    private void read_cascade_file(){
+        copyFile("haarcascade_frontalface_alt.xml");
+        copyFile("haarcascade_eye_tree_eyeglasses.xml");
+
+        Log.d(TAG, "read_cascade_file:");
+
+        cascadeClassifier_face = loadCascade( "haarcascade_frontalface_alt.xml");
+        Log.d(TAG, "read_cascade_file:");
+
+        cascadeClassifier_eye = loadCascade( "haarcascade_eye_tree_eyeglasses.xml");
+    }
+
+
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    mOpenCvCameraView.enableView();
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON,
+                WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_main);
+
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            //퍼미션 상태 확인
+            if (!hasPermissions(PERMISSIONS)) {
+
+                //퍼미션 허가 안되어있다면 사용자에게 요청
+                requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            }
+            else  read_cascade_file(); //추가
+        }
+        else  read_cascade_file(); //추가
+
+        mOpenCvCameraView = (CameraBridgeViewBase)findViewById(R.id.activity_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+        // 기존 코드 주석처리
+        //mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
+        mOpenCvCameraView.setCameraIndex(0); // front-camera(1),  back-camera(0)
+
+        mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+
+
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                try {
+                    getWriteLock();
+
+                    File path = new File(Environment.getExternalStorageDirectory() + "/Images/");
+                    path.mkdirs();
+                    File file = new File(path, "image.png");
+
+                    String filename = file.toString();
+
+                    Imgproc.cvtColor(matResult, matResult, Imgproc.COLOR_BGR2RGB, 4);
+                    boolean ret  = Imgcodecs.imwrite( filename, matResult);
+                    if ( ret ) Log.d(TAG, "SUCESS");
+                    else Log.d(TAG, "FAIL");
+
+
+                    Intent mediaScanIntent = new Intent( Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                    mediaScanIntent.setData(Uri.fromFile(file));
+                    sendBroadcast(mediaScanIntent);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+
+                releaseWriteLock();
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "onResume :: Internal OpenCV library not found.");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_2_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "onResum :: OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+
+        try {
+            getWriteLock();
+
+            matInput = inputFrame.rgba();
+
+            //if ( matResult != null ) matResult.release(); fix 2018. 8. 18
+
+            if (matResult == null)
+
+                matResult = new Mat(matInput.rows(), matInput.cols(), matInput.type());
+
+            // 기존 코드 주석처리
+            // ConvertRGBtoGray(matInput.getNativeObjAddr(), matResult.getNativeObjAddr());
+            Core.flip(matInput, matInput, 1);
+
+            detect(cascadeClassifier_face, cascadeClassifier_eye, matInput.getNativeObjAddr(),
+                    matResult.getNativeObjAddr());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        releaseWriteLock();
+
+        return matResult;
+    }
+
+
+
+    //여기서부턴 퍼미션 관련 메소드
+    static final int PERMISSIONS_REQUEST_CODE = 1000;
+    //String[] PERMISSIONS  = {"android.permission.CAMERA"};
+    String[] PERMISSIONS  = {"android.permission.CAMERA",
+            "android.permission.WRITE_EXTERNAL_STORAGE"};
+
+
+    private boolean hasPermissions(String[] permissions) {
+        int result;
+
+        //스트링 배열에 있는 퍼미션들의 허가 상태 여부 확인
+        for (String perms : permissions){
+
+            result = ContextCompat.checkSelfPermission(this, perms);
+
+            if (result == PackageManager.PERMISSION_DENIED){
+                //허가 안된 퍼미션 발견
+                return false;
+            }
+        }
+
+        //모든 퍼미션이 허가되었음
+        return true;
+    }
+
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch(requestCode){
+
+            case PERMISSIONS_REQUEST_CODE:
+                if (grantResults.length > 0) {
+                    boolean cameraPermissionAccepted = grantResults[0]
+                            == PackageManager.PERMISSION_GRANTED;
+
+                    // 기존 코드 주석처리
+
+                    // if (!cameraPermissionAccepted)
+
+                    //    showDialogForPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
+
+
+                    boolean writePermissionAccepted = grantResults[1]
+                            == PackageManager.PERMISSION_GRANTED;
+
+                    if (!cameraPermissionAccepted || !writePermissionAccepted) {
+                        showDialogForPermission("앱을 실행하려면 퍼미션을 허가하셔야합니다.");
+                        return;
+                    }else
+                    {
+                        read_cascade_file();
+                    }
+                }
+                break;
+        }
+    }
+
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private void showDialogForPermission(String msg) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder( MainActivity.this);
+        builder.setTitle("알림");
+        builder.setMessage(msg);
+        builder.setCancelable(false);
+        builder.setPositiveButton("예", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id){
+                requestPermissions(PERMISSIONS, PERMISSIONS_REQUEST_CODE);
+            }
+        });
+        builder.setNegativeButton("아니오", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface arg0, int arg1) {
+                finish();
+            }
+        });
+        builder.create().show();
     }
 
 
